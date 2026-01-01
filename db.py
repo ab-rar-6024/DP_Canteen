f944b22a42bc8ce991002e61d9c8f9bcce3e1281
Mohamed Abrar S A <samohamedabrar2005@gmail.com>
2026-01-01 12:35:04 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,10 @@ import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.environ.get("DB_HOST"),
-        database=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        port=os.environ.get("DB_PORT")
+        host=os.environ["DB_HOST"],
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        port=os.environ["DB_PORT"],
+        sslmode="require"   # 🔥 THIS IS THE FIX
     )
