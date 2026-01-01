61f1ff78895f3a21dc1858e6f4e52685b1feb21e
Mohamed Abrar S A <samohamedabrar2005@gmail.com>
2026-01-01 12:14:56 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
-import mysql.connector
+import psycopg2
+import os
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="canteen_system"
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT")
     )
